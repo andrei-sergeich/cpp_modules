0d8f3bfe41071cb34608aa637e2a469f5ec5173d
andrei <andrei-sergeich@yandex.ru>
2022-03-09 20:32:21 +0300

    m01ex06 done

---
+++ module01/ex06/main.cpp
@@ -0,0 +1,13 @@
+#include "Karen.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		Karen	karen;
+		karen.complain(argv[1]);
+	}
+	else
+		std::cout << "Usage: ./karenFilter <level>" << std::endl;
+	return 0;
+};
